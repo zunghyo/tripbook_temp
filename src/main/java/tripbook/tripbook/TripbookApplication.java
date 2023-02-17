9eb03ab914ad8d9003fd516f7157a464da480e9b
hyojeong <khj444453@gmail.com>
2023-02-17 11:22:55 +0900

    first commit

---
+++ src/main/java/tripbook/tripbook/TripbookApplication.java
@@ -0,0 +1,13 @@
+package tripbook.tripbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tripbook/tripbook/TripbookApplicationTests.java
@@ -0,0 +1,13 @@
+package tripbook.tripbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
